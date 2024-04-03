a330cfcc7791d505c7230cfda852d3b7b68b26d7
aukik <aukikaurnabx@gmail.com>
2024-04-04 05:44:14 +0600

    first commit

---
+++ js/heatmap.js
@@ -0,0 +1,9 @@
+// Replace "YOUR_GITHUB_USERNAME" with your actual GitHub username
+const gitHubUsername = "aukik";
+
+// Call the GitHubCalendar function and pass the required options
+GitHubCalendar(".calendar", gitHubUsername, {
+  responsive: true,
+  global_stats: false,
+  tooltips: true,
+});
