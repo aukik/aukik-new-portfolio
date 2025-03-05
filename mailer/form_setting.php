819795036fe2bab18779f666dd786134bc23621e
aukik <aukikaurnabx@gmail.com>
2025-03-06 05:07:22 +0600

    first updates

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from your portfolio"; //letter subject
-	$to = 'aurnab@bismoapps.com'; // Enter Your E-mail
+	$to = 'aukikaurnabx@gmail.com'; // Enter Your E-mail
 	$from = 'aukikaurnabx@gmail.com'; // Admin e-mail
 	$fromName = 'Aukik Aurnab'; // Your company name
 	$charset = 'UTF-8';
